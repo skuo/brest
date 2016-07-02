62e8d72f20093ad729032bacbbff376c15a55c51
Steve Kuo <steve.kuo@makerstudios.com>
2016-07-01 19:42:44 -0700

    Trying to get Spring Data endpoint to work

--- src/main/java/org/brest/payroll/DatabaseLoader.java
+++ src/main/java/org/brest/payroll/DatabaseLoader.java
@@ -1,14 +1,15 @@
 package org.brest.payroll;
 
-import org.brest.hello.GreetingController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DatabaseLoader implements CommandLineRunner {
     
-    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+    private static final Logger log = LoggerFactory.getLogger(DatabaseLoader.class);
     private final EmployeeRepository repository;
 
     @Autowired
